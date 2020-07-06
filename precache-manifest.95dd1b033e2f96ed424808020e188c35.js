61f4f36b513336e27c7f4e7bf77452e17cc8004e
James Manalang <jaesmanalang@gmail.com>
2020-07-06 17:42:31 +0800

    Updates

---
+++ precache-manifest.95dd1b033e2f96ed424808020e188c35.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8579787beadc29048badec2ac2c07cc",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "26b5d82b16189a00bac5",
+    "url": "/covid-19-tracker/static/css/main.46dfe0da.chunk.css"
+  },
+  {
+    "revision": "a7348c3008c339e113c7",
+    "url": "/covid-19-tracker/static/js/2.fac457b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-tracker/static/js/2.fac457b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26b5d82b16189a00bac5",
+    "url": "/covid-19-tracker/static/js/main.eb5023ba.chunk.js"
+  },
+  {
+    "revision": "69260950a26d9413a7f9",
+    "url": "/covid-19-tracker/static/js/runtime-main.2dc355e1.js"
+  },
+  {
+    "revision": "19e8ebe8d674ad79f0a706b593dd6019",
+    "url": "/covid-19-tracker/static/media/active.19e8ebe8.svg"
+  },
+  {
+    "revision": "d4f606ae3eda822465d702c2e2991fae",
+    "url": "/covid-19-tracker/static/media/deaths.d4f606ae.svg"
+  },
+  {
+    "revision": "a71ccda28af581b2c36e12afd5128cb7",
+    "url": "/covid-19-tracker/static/media/loader.a71ccda2.svg"
+  },
+  {
+    "revision": "602400e372db841f214727f71d86f1b3",
+    "url": "/covid-19-tracker/static/media/recovery.602400e3.svg"
+  },
+  {
+    "revision": "80a698f10d7d80e6d766e6455cc7697e",
+    "url": "/covid-19-tracker/static/media/virus.80a698f1.svg"
+  }
+]);
